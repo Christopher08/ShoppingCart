901e32a0f47410562fa8ef4d855516f30cc9750d
Christopher08 <tales182@hotmail.com>
2015-09-03 18:21:57 +1000

    Create config.php
    the DB connection

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+//fill in your data here
+$host = "";
+$db_name = "";
+$username = "";
+$password = "";
+
+ 
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+ 
+//to handle connection error
+catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
+?>
