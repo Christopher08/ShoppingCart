7fe6fe2a34ec1484b62b8dd28b9ef3c567eb06c6
Christopher08 <tales182@hotmail.com>
2015-09-03 18:53:28 +1000

    Create header.php
    the header

---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+// connect to database
+include 'config/db_connection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Shopping Cart: Paypal as Payment Gateway</title>
+ 
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+ 
+    <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+ 
+</head>
+<body style= background-color:#009cde;>
+ 
+    <?php include 'nav.php'; 
+    ?>
+ 
+    <!-- container -->
+    <div class="container">
+ 
+        <div class="page-header">
+            <h1>Shopping Cart: Paypal as Payment Gateway</h1>
+        </div>
